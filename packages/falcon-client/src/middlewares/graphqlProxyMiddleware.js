118dda0389148856d3d0a90935a92211b2375c4f
tutok <walas.marcin@gmail.com>
2019-10-08 13:52:44 +0200

    feat(hook-errors): added support for errros occured durring communication when proxing

--- packages/falcon-client/src/middlewares/graphqlProxyMiddleware.js
+++ packages/falcon-client/src/middlewares/graphqlProxyMiddleware.js
@@ -1,13 +1,24 @@
+import { toApolloError } from 'apollo-server-errors';
 import { ProxyRequest } from '../service/ProxyRequest';
 
-// GraphQL Proxy to Falcon-Server
-export default httpLinkUri => async ctx => {
-  const result = await ProxyRequest(httpLinkUri, ctx);
+/**
+ * GraphQL requests proxy to (Falcon-Server)
+ * @param {string} graphQLServerUrl
+ */
+export default graphQLServerUrl => {
+  return async ctx => {
+    try {
+      const result = await ProxyRequest(graphQLServerUrl, ctx);
 
-  // `result.body` will set a proper `content-encoding` header,
-  // otherwise - it leads to a "net::ERR_CONTENT_DECODING_FAILED" error
-  ctx.remove('content-encoding');
-
-  ctx.status = result.status;
-  ctx.body = result.body;
+      ctx.status = result.status;
+      ctx.body = result.body;
+    } catch (error) {
+      ctx.status = 200;
+      ctx.body = toApolloError(error, error.code);
+    } finally {
+      // `result.body` will set a proper `content-encoding` header,
+      // otherwise - it leads to a "net::ERR_CONTENT_DECODING_FAILED" error
+      ctx.remove('content-encoding');
+    }
+  };
 };
