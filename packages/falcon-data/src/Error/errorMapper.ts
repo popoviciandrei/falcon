f9eb1534859e3191f5f4067a0f6044d32a342169
tutok <walas.marcin@gmail.com>
2019-10-08 12:18:28 +0200

    feat(hook-errors): added isUserError and userErrorMapper

--- packages/falcon-data/src/Error/errorMapper.ts
+++ packages/falcon-data/src/Error/errorMapper.ts
@@ -1,6 +1,42 @@
+import { ApolloError, isApolloError } from 'apollo-client';
+import { codes } from '@deity/falcon-errors';
+
 export type ErrorModel = {
   message: string;
   code: string;
   /** path to property (on operation input or operation output) on which error occurs */
   path?: any;
+};
+
+export const isUserError = <TError extends Error>(error: TError): boolean => {
+  if (isApolloError(error)) {
+    const { graphQLErrors, networkError } = error;
+    if (!networkError && Array.isArray(graphQLErrors) && graphQLErrors.length > 0) {
+      return true;
+    }
+  }
+
+  return false;
+};
+
+export const userErrorToErrorModelList = <TError extends Error>(error: TError): ErrorModel[] => {
+  if (isUserError(error)) {
+    const { graphQLErrors } = (error as any) as ApolloError;
+
+    return graphQLErrors.reduce<ErrorModel[]>((result, { message, extensions = {} }) => {
+      if (extensions.code === codes.BAD_USER_INPUT && extensions.exception) {
+        const userInputErrors = Object.keys(extensions.exception).map(x => ({
+          message: extensions.exception[x],
+          code: extensions.code || 'UNKNOWN',
+          path: x
+        }));
+
+        return [...result, ...userInputErrors];
+      }
+
+      return [...result, { message, code: extensions.code }];
+    }, []);
+  }
+
+  return [];
 };
