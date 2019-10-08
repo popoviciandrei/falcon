a0cfa12c3b77485f4fd3c5b049428a2894d1dcf1
tutok <walas.marcin@gmail.com>
2019-10-08 09:38:08 +0200

    feat(hook-errors): make sure that ProxyRequest will not handle any errors

--- packages/falcon-client/src/service/ProxyRequest.js
+++ packages/falcon-client/src/service/ProxyRequest.js
@@ -1,26 +1,31 @@
-import url from 'url';
+import Url from 'url';
 import fetch from 'node-fetch';
 
-export async function ProxyRequest(targetUrl, ctx) {
-  const { method, header } = ctx.request;
-  const response = await fetch(targetUrl, {
-    method,
+/**
+ * @param {string} url
+ * @param {import('koa-router').IRouterContext} ctx
+ */
+export async function ProxyRequest(url, ctx) {
+  const { request, req } = ctx;
+
+  return fetch(url, {
+    method: request.method,
     headers: {
-      ...header,
+      ...request.header,
       // Overriding Host header with the target server
-      host: url.parse(targetUrl).host
+      host: Url.parse(url).host
     },
-    body: method === 'POST' ? ctx.req : undefined
+    body: request.method === 'POST' ? req : undefined
+  }).then(response => {
+    response.headers.forEach((value, name) => ctx.set(name, value));
+
+    // TODO: we should not change `ctx` here!
+
+    // https://github.com/bitinn/node-fetch/blob/master/src/headers.js#L120
+    // node-fetch returns `set-cookie` headers concatenated with ", " (which is invalid)
+    // for this reason we get a list of "raw" headers and set them to the response
+    ctx.set('set-cookie', response.headers.raw()['set-cookie'] || []);
+
+    return response;
   });
-
-  response.headers.forEach((value, name) => {
-    ctx.set(name, value);
-  });
-
-  // https://github.com/bitinn/node-fetch/blob/master/src/headers.js#L120
-  // node-fetch returns `set-cookie` headers concatenated with ", " (which is invalid)
-  // for this reason we get a list of "raw" headers and set them to the response
-  ctx.set('set-cookie', response.headers.raw()['set-cookie'] || []);
-
-  return response;
 }
