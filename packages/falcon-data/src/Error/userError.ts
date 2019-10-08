15815e24c85ce9e68ff77cb6cd03737aae79ab8e
tutok <walas.marcin@gmail.com>
2019-10-08 13:26:24 +0200

    feat(hook-errors): added function which extracts user erros if any

--- packages/falcon-data/src/Error/userError.ts
+++ packages/falcon-data/src/Error/userError.ts
@@ -40,3 +40,11 @@ export const userErrorToErrorModelList = <TError extends Error>(error: TError): 
 
   return [];
 };
+
+export const getUserErrors = <TError extends Error>(error: TError): ErrorModel[] => {
+  if (isUserError(error)) {
+    return userErrorToErrorModelList(error);
+  }
+
+  return [];
+};
