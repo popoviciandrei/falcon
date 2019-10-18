a1e7993de418e4fd97ecd35e4efd65629849418b
tutok <walas.marcin@gmail.com>
2019-10-18 11:06:55 +0200

    feat(hook-errors): added description for useThrowError hook

--- packages/falcon-data/src/Error/useThrowError.ts
+++ packages/falcon-data/src/Error/useThrowError.ts
@@ -1,5 +1,9 @@
 import { useState, useEffect } from 'react';
 
+/**
+ * React Hook which allows throwing an error (catchable via ErrorBoundary) from another hook or promise
+ * @see https://github.com/facebook/react/issues/14981
+ */
 export function useThrowError() {
   const [error, setError] = useState<Error>();
 
