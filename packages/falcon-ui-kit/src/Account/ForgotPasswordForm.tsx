9e90dd29940ddbbfb7ccb4bfcc0fe9bf1d0c5931
Nimo Beeren <nimo@deity.io>
2019-10-22 16:18:30 +0200

    improvement: remove unused import

--- packages/falcon-ui-kit/src/Account/ForgotPasswordForm.tsx
+++ packages/falcon-ui-kit/src/Account/ForgotPasswordForm.tsx
@@ -3,7 +3,7 @@ import { T } from '@deity/falcon-i18n';
 import { Text } from '@deity/falcon-ui';
 import { ForgotPasswordFormProvider } from '@deity/falcon-front-kit';
 import { ErrorSummary } from '../Error';
-import { FormField, Form, FormSubmit, FormProps, FormErrorSummary } from '../Forms';
+import { FormField, Form, FormSubmit, FormProps } from '../Forms';
 
 export type ForgetPasswordFormProps = Partial<FormProps> & {
   onSuccess?: () => void;
