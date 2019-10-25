f3dd0806ef8476c873f5b5a18f4285748b638f8e
Eddy Vinck <eddy@deity.io>
2019-10-25 13:10:54 +0200

    improvement: add a generator meta tag so we can check if they are using Falcon

--- packages/falcon-client/src/components/HtmlHead.js
+++ packages/falcon-client/src/components/HtmlHead.js
@@ -3,7 +3,11 @@ import PropTypes from 'prop-types';
 import { Helmet } from 'react-helmet-async';
 
 export default function HtmlHead({ htmlLang }) {
-  return <Helmet htmlAttributes={{ lang: htmlLang }} />;
+  return (
+    <Helmet htmlAttributes={{ lang: htmlLang }}>
+      <meta name="generator" content="DEITY Falcon" />
+    </Helmet>
+  );
 }
 
 HtmlHead.propTypes = {
