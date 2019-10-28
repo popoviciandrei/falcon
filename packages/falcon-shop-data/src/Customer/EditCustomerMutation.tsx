0c57ec3034de361a98e69df9bfd96f9788493965
tutok <walas.marcin@gmail.com>
2019-10-28 09:47:14 +0100

    feat(example-app-improvements): added editcustomermutation hook

--- packages/falcon-shop-data/src/Customer/EditCustomerMutation.tsx
+++ packages/falcon-shop-data/src/Customer/EditCustomerMutation.tsx
@@ -1,8 +1,9 @@
 import gql from 'graphql-tag';
+import { useMutation, MutationHookOptions } from '@apollo/react-hooks';
 import { Mutation, OperationInput } from '@deity/falcon-data';
 import { Customer, EditCustomerInput } from '@deity/falcon-shop-extension';
 
-export const EDIT_CUSTOMER = gql`
+export const EDIT_CUSTOMER_MUTATION = gql`
   mutation EditCustomer($input: CustomerInput!) {
     editCustomer(input: $input) {
       id
@@ -16,7 +17,11 @@ export type EditCustomerResponse = {
 
 export class EditCustomerMutation extends Mutation<EditCustomerResponse, OperationInput<EditCustomerInput>> {
   static defaultProps = {
-    mutation: EDIT_CUSTOMER,
+    mutation: EDIT_CUSTOMER_MUTATION,
     refetchQueries: ['Customer', 'CustomerWithAddresses']
   };
 }
+
+export const useEditCustomerMutation = (
+  options: MutationHookOptions<EditCustomerResponse, OperationInput<EditCustomerInput>> = {}
+) => useMutation(EDIT_CUSTOMER_MUTATION, options);
