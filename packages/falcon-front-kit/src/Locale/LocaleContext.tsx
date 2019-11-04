b9ab20e07e61b2dd5614d5d025e790a30d27dcab
tutok <walas.marcin@gmail.com>
2019-11-04 14:06:49 +0100

    feat(693): removed currency from locale context, also added useLocale hook

--- packages/falcon-front-kit/src/Locale/LocaleContext.tsx
+++ packages/falcon-front-kit/src/Locale/LocaleContext.tsx
@@ -1,49 +1,33 @@
-import React from 'react';
+import React, { useContext } from 'react';
 import { BackendConfigQuery } from '@deity/falcon-shop-data';
 import { ClientConfigQuery } from '../ClientConfig';
 import { dateTimeFormatFactory, DateTimeFormatOptions } from './dateTimeFormat';
-import { priceFormatFactory, PriceFormatOptions } from './priceFormat';
 
 export type LocaleContextType = {
   locale: string;
   localeFallback: string;
-  currency: string;
-  priceFormat: ReturnType<typeof priceFormatFactory>;
   dateTimeFormat: ReturnType<typeof dateTimeFormatFactory>;
 };
 
 const LocaleContext = React.createContext<LocaleContextType>({} as any);
 
 export type LocaleProviderProps = {
-  currency?: string;
-  priceFormatOptions?: PriceFormatOptions;
   dateTimeFormatOptions?: DateTimeFormatOptions;
 };
-export const LocaleProvider: React.SFC<LocaleProviderProps> = ({
-  children,
-  priceFormatOptions = {},
-  dateTimeFormatOptions = {},
-  ...props
-}) => (
+export const LocaleProvider: React.SFC<LocaleProviderProps> = ({ children, dateTimeFormatOptions = {} }) => (
   <ClientConfigQuery>
     {({ data: { clientConfig } }) => (
       <BackendConfigQuery>
         {({ data: { backendConfig } }) => {
-          const { activeLocale, shop } = backendConfig;
+          const { activeLocale } = backendConfig;
 
           const localeFallback = clientConfig.i18n.lng;
-          const currency = props.currency || shop.activeCurrency || 'EUR';
 
           return (
             <LocaleContext.Provider
               value={{
                 locale: activeLocale,
                 localeFallback,
-                currency,
-                priceFormat: priceFormatFactory([priceFormatOptions.locale, activeLocale, localeFallback], {
-                  currency,
-                  ...priceFormatOptions
-                }),
                 dateTimeFormat: dateTimeFormatFactory([dateTimeFormatOptions.locale, activeLocale, localeFallback], {
                   ...dateTimeFormatOptions
                 })
@@ -58,15 +42,11 @@ export const LocaleProvider: React.SFC<LocaleProviderProps> = ({
   </ClientConfigQuery>
 );
 
+export const useLocale = (): LocaleContextType => useContext(LocaleContext);
+
+export type LocaleRenderProps = LocaleContextType;
 export type LocaleProps = {
   children: (props: LocaleRenderProps) => any;
 };
-export type LocaleRenderProps = {
-  locale: string;
-  currency: string;
-  priceFormat: ReturnType<typeof priceFormatFactory>;
-  dateTimeFormat: ReturnType<typeof dateTimeFormatFactory>;
-};
-export const Locale: React.SFC<LocaleProps> = ({ children }) => (
-  <LocaleContext.Consumer>{({ localeFallback, ...props }) => children({ ...props })}</LocaleContext.Consumer>
-);
+
+export const Locale: React.SFC<LocaleProps> = ({ children }) => children({ ...useLocale() });
