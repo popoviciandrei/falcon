de40ebfc5ad77e4311dc78bcce5dc9c11239c66f
tutok <walas.marcin@gmail.com>
2019-11-07 13:09:48 +0100

    feat(checkout-providers): added posiblity to select full County object into CountyPicker

--- packages/falcon-ui-kit/src/Country/CountryPicker.tsx
+++ packages/falcon-ui-kit/src/Country/CountryPicker.tsx
@@ -1,11 +1,33 @@
-import React from 'react';
+import React, { useMemo } from 'react';
 import { Country } from '@deity/falcon-shop-extension';
 import { SelectInput, SelectInputProps } from '../Forms';
 
-export type CountryPickerProps = Omit<SelectInputProps, 'options'> & {
-  options: Pick<Country, 'code' | 'localName'>[];
+export type CountryPickerItem = Pick<Country, 'id' | 'code' | 'localName'>;
+
+export type CountryPickerProps = Omit<SelectInputProps, 'options' | 'onChange'> & {
+  options: CountryPickerItem[];
+  value: CountryPickerItem;
+  onChange: (value: CountryPickerItem) => any;
 };
 
-export const CountryPicker: React.SFC<CountryPickerProps> = ({ options, ...rest }) => (
-  <SelectInput {...rest} options={options.map(({ code, localName }) => ({ value: code, label: localName }))} />
-);
+export const CountryPicker: React.SFC<CountryPickerProps> = ({ value, options, onChange, ...rest }) => {
+  const selectOptions = useMemo(
+    () =>
+      options.map(({ code, localName }) => ({
+        value: code,
+        label: localName || code
+      })),
+    [options]
+  );
+
+  return (
+    <SelectInput
+      {...rest}
+      value={value ? value.code : undefined}
+      options={selectOptions}
+      onChange={e => {
+        return onChange(options.find(x => x.code === e.target.value));
+      }}
+    />
+  );
+};
