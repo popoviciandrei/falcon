9f05420021d496fd0fabf138bbd5851179d4bd12
tutok <walas.marcin@gmail.com>
2019-11-07 13:31:33 +0100

    feat(checkout): added meemo into Address Picker

--- examples/shop-with-blog/client/src/pages/shop/Checkout/components/AddressPicker.js
+++ examples/shop-with-blog/client/src/pages/shop/Checkout/components/AddressPicker.js
@@ -1,13 +1,16 @@
-import React from 'react';
+import React, { useMemo } from 'react';
 import { addressToString } from '@deity/falcon-front-kit';
 import { Picker } from '@deity/falcon-ui-kit';
 
 export const AddressPicker = ({ options, selected, onChange }) => {
-  const noSelectionOption = { value: 'Other', label: 'Other' };
+  const pickerOptions = useMemo(
+    () => [...options.map(value => ({ value, label: addressToString(value) })), { value: 'Other', label: 'Other' }],
+    [options]
+  );
 
   return (
     <Picker
-      options={[...options.map(value => ({ value, label: addressToString(value) })), noSelectionOption]}
+      options={pickerOptions}
       selected={selected && typeof selected === 'object' ? addressToString(selected) : selected}
       onChange={value => onChange(value)}
     />
