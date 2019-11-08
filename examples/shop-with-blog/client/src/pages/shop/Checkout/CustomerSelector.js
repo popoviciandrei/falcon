df7cc33d3e7e38a922dfa7787405605324a9a864
tutok <walas.marcin@gmail.com>
2019-11-08 13:45:20 +0100

    feat(checkout-providers): converted CustomerSelector into function component

--- examples/shop-with-blog/client/src/pages/shop/Checkout/CustomerSelector.js
+++ examples/shop-with-blog/client/src/pages/shop/Checkout/CustomerSelector.js
@@ -49,114 +49,65 @@ EmailForm.propTypes = {
   email: PropTypes.string
 };
 
-class EmailSection extends React.Component {
-  constructor(props) {
-    super(props);
+export const EmailSection = props => {
+  const { open, data: { customer } = {}, onEditRequested, setEmail, email } = props;
 
-    let email = props.email || '';
-
-    if (props.data && props.data.customer) {
-      ({ email } = props.data.customer);
-      props.setEmail(email);
-    }
-
-    this.state = {
-      email: props.email,
-      getPrevProps: () => this.props
-    };
+  if (customer) {
+    setEmail(customer.email);
+    // TODO: go to next step;
   }
 
-  static getDerivedStateFromProps(nextProps, prevState) {
-    const {
-      data: { customer: prevCustomer }
-    } = prevState.getPrevProps();
-    const {
-      data: { customer: nextCustomer }
-    } = nextProps;
-
-    const { email: prevCustomerEmail } = prevCustomer || {};
-    const { email: nextCustomerEmail } = nextCustomer || {};
-
-    if (prevCustomerEmail !== nextCustomerEmail) {
-      // user has signed in or out so we have to trigger setEmail() with the new value
-      nextProps.setEmail(nextCustomerEmail);
-
-      // if there's no email in nextProps then customer just signed out - in that case we trigger
-      // edit process so wizard switches to correct section
-      if (!nextCustomerEmail) {
-        nextProps.onEditRequested();
-      }
-
-      return {
-        ...prevState,
-        email: nextCustomerEmail || ''
-      };
-    }
-
-    if (nextProps.email && nextProps.email !== prevState.email) {
-      return {
-        ...prevState,
-        email: nextProps.email
-      };
-    }
-
-    return null;
-  }
-
-  render() {
-    const { open, data, onEditRequested } = this.props;
-    const isSignedIn = !!data.customer;
-
-    return (
-      <I18n>
-        {t => (
-          <Details open={open}>
-            {!open && (
-              <SignOutMutation>
-                {signOut => (
-                  <SectionHeader
-                    title={t('customerSelector.title')}
-                    editLabel={t(isSignedIn ? 'customerSelector.signOut' : 'customerSelector.edit')}
-                    onActionClick={isSignedIn ? signOut : onEditRequested}
-                    complete
-                    summary={<Text>{this.state.email}</Text>}
-                  />
-                )}
-              </SignOutMutation>
-            )}
-            {open && (
-              <React.Fragment>
-                <SectionHeader title={t('customerSelector.title')} />
-                <DetailsContent>
-                  <Text>
-                    <T id="customerSelector.guestPrompt" />
-                  </Text>
-                  <EmailForm email={this.state.email} setEmail={this.props.setEmail} />
-                  <Text>
-                    <T id="customerSelector.or" />
-                    <OpenSidebarMutation>
-                      {openSidebar => (
-                        <Link
-                          mx="xs"
-                          color="primary"
-                          onClick={() => openSidebar({ variables: { contentType: SIDEBAR_TYPE.account } })}
-                        >
-                          <T id="customerSelector.signInLink" />
-                        </Link>
-                      )}
-                    </OpenSidebarMutation>
-                    <T id="customerSelector.ifAlreadyRegistered" />
-                  </Text>
-                </DetailsContent>
-              </React.Fragment>
-            )}
-          </Details>
-        )}
-      </I18n>
-    );
-  }
-}
-
+  return (
+    <I18n>
+      {t => (
+        <Details open={open}>
+          {!open && (
+            <SignOutMutation>
+              {signOut => (
+                <SectionHeader
+                  title={t('customerSelector.title')}
+                  editLabel={t(customer ? 'customerSelector.signOut' : 'customerSelector.edit')}
+                  onActionClick={customer ? signOut : onEditRequested}
+                  complete
+                  summary={<Text>{email}</Text>}
+                />
+              )}
+            </SignOutMutation>
+          )}
+          {open && (
+            <React.Fragment>
+              <SectionHeader title={t('customerSelector.title')} />
+              <DetailsContent>
+                <Text>
+                  <T id="customerSelector.guestPrompt" />
+                </Text>
+                <EmailForm email={email} setEmail={setEmail} />
+                <Text>
+                  <T id="customerSelector.or" />
+                  <OpenSidebarMutation>
+                    {openSidebar => (
+                      <Link
+                        mx="xs"
+                        color="primary"
+                        onClick={() => openSidebar({ variables: { contentType: SIDEBAR_TYPE.account } })}
+                      >
+                        <T id="customerSelector.signInLink" />
+                      </Link>
+                    )}
+                  </OpenSidebarMutation>
+                  <T id="customerSelector.ifAlreadyRegistered" />
+                </Text>
+              </DetailsContent>
+            </React.Fragment>
+          )}
+        </Details>
+      )}
+    </I18n>
+  );
+};
+EmailSection.defaultProps = {
+  email: ''
+};
 EmailSection.propTypes = {
   // data form GET_CUSTOMER query
   data: PropTypes.shape({}),
@@ -168,10 +119,6 @@ EmailSection.propTypes = {
   onEditRequested: PropTypes.func,
   // flag that indicates if the section is currently open
   open: PropTypes.bool
-};
-
-EmailSection.defaultProps = {
-  email: ''
 };
 
 export default graphql(GET_CUSTOMER)(EmailSection);
