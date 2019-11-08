51fef83e7d25d36286e644886c322c99d9e86647
tutok <walas.marcin@gmail.com>
2019-11-08 15:07:52 +0100

    feat(checkout-providers): cleanup

--- packages/falcon-front-kit/src/Checkout/CheckoutConsumer.tsx
+++ packages/falcon-front-kit/src/Checkout/CheckoutConsumer.tsx
@@ -1,7 +1,7 @@
 import React, { useContext } from 'react';
 import { CheckoutContext, CheckoutProviderRenderProps } from './CheckoutContext';
 
-export const useCheckoutContext = () => useContext(CheckoutContext);
+export const useCheckout = () => useContext(CheckoutContext);
 
 export const Checkout: React.SFC<{ children: (props: CheckoutProviderRenderProps) => any }> = ({ children }) =>
-  children({ ...useCheckoutContext() });
+  children({ ...useCheckout() });
