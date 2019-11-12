bac7efba1b148182f45a4550ac1d2fe2915defac
tutok <walas.marcin@gmail.com>
2019-11-12 10:56:35 +0100

    feat(checkout-providers): added customer query into ShippingAddressSection

--- examples/shop-with-blog/client/src/pages/shop/Checkout/ShippingAddressSection.js
+++ examples/shop-with-blog/client/src/pages/shop/Checkout/ShippingAddressSection.js
@@ -1,18 +1,19 @@
 import React, { useState } from 'react';
 import PropTypes from 'prop-types';
-import { Details, DetailsContent, Button } from '@deity/falcon-ui';
 import { I18n } from '@deity/falcon-i18n';
+import { Details, DetailsContent, Button } from '@deity/falcon-ui';
 import {
   SetShippingAddressFormProvider,
   checkoutAddressToSetCheckoutAddressFormValues,
   useCheckout
 } from '@deity/falcon-front-kit';
 import { AddressDetails, Form, AddressFormFields, ErrorSummary, Loader } from '@deity/falcon-ui-kit';
+import { CustomerWithAddressesQuery } from '@deity/falcon-shop-data';
 import SectionHeader from './CheckoutSectionHeader';
 import { AddressPicker } from './components';
 
 export const ShippingAddressSection = props => {
-  const { open, title, onEditRequested, submitLabel, availableAddresses } = props;
+  const { open, title, onEditRequested, submitLabel } = props;
   const { setShippingAddress, values } = useCheckout();
   const [address, setAddress] = useState(values.shippingAddress);
   const isAddressOther = address && !address.id;
@@ -40,30 +41,37 @@ export const ShippingAddressSection = props => {
     <Details open={open}>
       {header}
       <DetailsContent>
-        <SetShippingAddressFormProvider onSuccess={setShippingAddress}>
-          {({ isSubmitting, setValues, status: { error } }) => (
-            <React.Fragment>
-              {availableAddresses.length > 0 && (
-                <AddressPicker
-                  options={availableAddresses}
-                  selected={address}
-                  onChange={x => {
-                    setAddress(x);
-                    setValues(x ? checkoutAddressToSetCheckoutAddressFormValues(x) : {});
-                  }}
-                />
+        <CustomerWithAddressesQuery>
+          {({ data: { customer } }) => (
+            <SetShippingAddressFormProvider onSuccess={setShippingAddress}>
+              {({ isSubmitting, setValues, status: { error } }) => (
+                <Form id="shipping-address" i18nId="addressForm" my="sm">
+                  {isSubmitting && <Loader variant="overlay" />}
+                  {customer && customer.addresses.length && (
+                    <AddressPicker
+                      options={customer.addresses}
+                      selected={address}
+                      onChange={x => {
+                        setAddress(x);
+                        setValues(x ? checkoutAddressToSetCheckoutAddressFormValues(x) : {});
+                        // if (x) {
+                        //   setValues(checkoutAddressToSetCheckoutAddressFormValues(x));
+                        // } else {
+                        //   resetForm();
+                        // }
+                      }}
+                    />
+                  )}
+                  {((customer && customer.addresses.length) || !address || isAddressOther) && (
+                    <AddressFormFields autoCompleteSection="shipping-address" />
+                  )}
+                  <Button type="submit">{submitLabel}</Button>
+                  <ErrorSummary errors={error} />
+                </Form>
               )}
-              <Form id="shipping-address" i18nId="addressForm" my="sm">
-                {isSubmitting && <Loader variant="overlay" />}
-                {(availableAddresses.length === 0 || !address || isAddressOther) && (
-                  <AddressFormFields autoCompleteSection="shipping-address" />
-                )}
-                <Button type="submit">{submitLabel}</Button>
-                <ErrorSummary errors={error} />
-              </Form>
-            </React.Fragment>
+            </SetShippingAddressFormProvider>
           )}
-        </SetShippingAddressFormProvider>
+        </CustomerWithAddressesQuery>
       </DetailsContent>
     </Details>
   );
@@ -73,15 +81,11 @@ ShippingAddressSection.propTypes = {
   open: PropTypes.bool,
   // title of the section
   title: PropTypes.string,
-  // currently selected address
-  selectedAddress: PropTypes.shape({}),
   // callback that should be called when user requests edit of this particular section
   onEditRequested: PropTypes.func,
   // flag indicates if "use the same address" is selected - if so then address form is hidden
   // label for submit button
   submitLabel: PropTypes.string,
-  // list of available addresses to pick from - if not passed then address selection field won't be presented
-  availableAddresses: PropTypes.arrayOf(PropTypes.shape({})),
   // default selected address - address that should be selected when address picker is shown
   defaultSelected: PropTypes.shape({})
 };
