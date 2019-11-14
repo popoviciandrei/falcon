017a43dd88cef8ccbe230b6bdcce148aacbae3d1
tutok <walas.marcin@gmail.com>
2019-11-14 22:11:19 +0100

    feat(checkout-providers): added set shipping and payment method hooks

---
+++ packages/falcon-front-kit/src/Checkout/SetPaymentMethod.tsx
@@ -0,0 +1,33 @@
+import React, { useState, useCallback } from 'react';
+import { CheckoutDetailsInput } from '@deity/falcon-shop-extension';
+import { useSetPaymentMethodMutation, SetPaymentMethodResponse } from '@deity/falcon-shop-data';
+import { useCheckoutState } from './CheckoutState';
+import { CheckoutOperation, CheckoutOperationHook } from './CheckoutOperation';
+
+export const useSetPaymentMethod: CheckoutOperationHook<SetPaymentMethodResponse, CheckoutDetailsInput> = () => {
+  const [method, setMethod] = useState<CheckoutDetailsInput>();
+  const { setLoading, setPaymentMethod } = useCheckoutState();
+  const [mutation, mutationResult] = useSetPaymentMethodMutation({
+    onCompleted: () => {
+      setPaymentMethod(method);
+      setLoading(false);
+    }
+  });
+
+  return [
+    useCallback(async (input: CheckoutDetailsInput, options) => {
+      setLoading(true);
+      setMethod(input);
+
+      return mutation({ ...options, variables: { input } });
+    }, []),
+    mutationResult
+  ];
+};
+
+export type SetPaymentMethodProps = {
+  children: CheckoutOperation<SetPaymentMethodResponse, CheckoutDetailsInput>;
+};
+export const SetPaymentMethod: React.FC<SetPaymentMethodProps> = ({ children }) => {
+  return children(...useSetPaymentMethod());
+};
---
+++ packages/falcon-front-kit/src/Checkout/SetShippingMethod.tsx
@@ -0,0 +1,33 @@
+import React, { useState, useCallback } from 'react';
+import { CheckoutDetailsInput } from '@deity/falcon-shop-extension';
+import { useSetShippingMethodMutation, SetShippingMethodResponse } from '@deity/falcon-shop-data';
+import { useCheckoutState } from './CheckoutState';
+import { CheckoutOperation, CheckoutOperationHook } from './CheckoutOperation';
+
+export const useSetShippingMethod: CheckoutOperationHook<SetShippingMethodResponse, CheckoutDetailsInput> = () => {
+  const [method, setMethod] = useState<CheckoutDetailsInput>();
+  const { setLoading, setShippingMethod } = useCheckoutState();
+  const [mutation, mutationResult] = useSetShippingMethodMutation({
+    onCompleted: () => {
+      setShippingMethod(method);
+      setLoading(false);
+    }
+  });
+
+  return [
+    useCallback(async (input: CheckoutDetailsInput, options) => {
+      setLoading(true);
+      setMethod(input);
+
+      return mutation({ ...options, variables: { input } });
+    }, []),
+    mutationResult
+  ];
+};
+
+export type SetShippingMethodProps = {
+  children: CheckoutOperation<SetShippingMethodResponse, CheckoutDetailsInput>;
+};
+export const SetShippingMethod: React.FC<SetShippingMethodProps> = ({ children }) => {
+  return children(...useSetShippingMethod());
+};
