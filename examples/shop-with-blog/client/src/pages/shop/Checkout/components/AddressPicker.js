98b6725090a579b5df7974534ba49f9280c7f863
tutok <walas.marcin@gmail.com>
2019-11-14 23:40:09 +0100

    feat(checkout-providers): simplified addressPicker

--- examples/shop-with-blog/client/src/pages/shop/Checkout/components/AddressPicker.js
+++ examples/shop-with-blog/client/src/pages/shop/Checkout/components/AddressPicker.js
@@ -1,18 +1,16 @@
-import React, { useMemo, useState } from 'react';
+import React, { useMemo } from 'react';
 import { addressToString } from '@deity/falcon-front-kit';
 import { Picker } from '@deity/falcon-ui-kit';
 
 export const isAddressCustom = address => !address || (address && !address.id);
 
 export const AddressPicker = ({ options, selected, onChange }) => {
-  const [customAddress] = useState(isAddressCustom(selected) ? selected : undefined);
-
   const pickerOptions = useMemo(() => {
-    return [...options, customAddress].map(value => ({
+    return [...options, isAddressCustom(selected) ? selected : undefined].map(value => ({
       value,
       label: isAddressCustom(value) ? 'Other' : addressToString(value)
     }));
-  }, [options, customAddress]);
+  }, [options]);
 
   return (
     <Picker
