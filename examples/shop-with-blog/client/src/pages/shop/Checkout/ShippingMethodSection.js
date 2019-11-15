4917e21da136aae203f3bf32b1d63806d0e55e86
tutok <walas.marcin@gmail.com>
2019-11-15 11:36:21 +0100

    feat(checkout-providers): using SetShippingMethod

--- examples/shop-with-blog/client/src/pages/shop/Checkout/ShippingMethodSection.js
+++ examples/shop-with-blog/client/src/pages/shop/Checkout/ShippingMethodSection.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import { I18n, T } from '@deity/falcon-i18n';
+import { ShippingMethodListQuery } from '@deity/falcon-shop-data';
+import { SetShippingMethod } from '@deity/falcon-front-kit';
 import { Label, FlexLayout, Details, DetailsContent, Text, Radio, Box, Button } from '@deity/falcon-ui';
 import { Price, ErrorSummary } from '@deity/falcon-ui-kit';
 import SectionHeader from './CheckoutSectionHeader';
@@ -40,13 +42,8 @@ class ShippingSection extends React.Component {
 
   onShippingSelected = selectedShipping => this.setState({ selectedShipping });
 
-  submitShipping = () => {
-    const { methodCode: method, ...data } = this.state.selectedShipping;
-    this.props.setShipping({ method, data });
-  };
-
   render() {
-    const { open, onEditRequested, availableShippingMethods, selectedShipping, errors } = this.props;
+    const { open, onEditRequested, selectedShipping, errors } = this.props;
     let header;
 
     if (!open && selectedShipping) {
@@ -70,24 +67,44 @@ class ShippingSection extends React.Component {
     return (
       <Details open={open}>
         {header}
-        <DetailsContent>
-          {availableShippingMethods.length === 0 ? (
-            <Text color="error" mb="sm">
-              <T id="checkout.noShippingMethodsAvailable" />
-            </Text>
-          ) : (
-            <ShippingSelector
-              availableShippingOptions={availableShippingMethods}
-              onShippingSelected={this.onShippingSelected}
-            />
-          )}
-          <ErrorSummary errors={errors} />
-          {availableShippingMethods.length > 0 && (
-            <Button disabled={!this.state.selectedShipping} onClick={this.submitShipping}>
-              <T id="continue" />
-            </Button>
-          )}
-        </DetailsContent>
+        {open && (
+          <DetailsContent>
+            <ShippingMethodListQuery>
+              {({ data: { shippingMethodList } }) => {
+                if (shippingMethodList.length === 0) {
+                  return (
+                    <Text color="error" mb="sm">
+                      <T id="checkout.noShippingMethodsAvailable" />
+                    </Text>
+                  );
+                }
+
+                return (
+                  <SetShippingMethod>
+                    {setShipping => (
+                      <React.Fragment>
+                        <ShippingSelector
+                          availableShippingOptions={shippingMethodList}
+                          onShippingSelected={this.onShippingSelected}
+                        />
+                        <Button
+                          disabled={!this.state.selectedShipping}
+                          onClick={() => {
+                            const { methodCode: method, ...data } = this.state.selectedShipping;
+                            setShipping({ method, data });
+                          }}
+                        >
+                          <T id="continue" />
+                        </Button>
+                        <ErrorSummary errors={errors} />
+                      </React.Fragment>
+                    )}
+                  </SetShippingMethod>
+                );
+              }}
+            </ShippingMethodListQuery>
+          </DetailsContent>
+        )}
       </Details>
     );
   }
@@ -96,20 +113,10 @@ class ShippingSection extends React.Component {
 ShippingSection.propTypes = {
   // flag that indicates if the section is currently open
   open: PropTypes.bool,
-  // all available shipping methods
-  availableShippingMethods: PropTypes.arrayOf(PropTypes.shape({})),
   // callback that should be called when user requests edit of this particular section
   onEditRequested: PropTypes.func,
   // currently selected shipping method
-  selectedShipping: PropTypes.shape({}),
-  // callback that sets selected shipping method
-  setShipping: PropTypes.func,
-  // errors passed from outside that should be displayed for this section
-  errors: PropTypes.arrayOf(
-    PropTypes.shape({
-      message: PropTypes.string
-    })
-  )
+  selectedShipping: PropTypes.shape({})
 };
 
 export default ShippingSection;
