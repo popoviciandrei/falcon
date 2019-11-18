41156cafc0ab6e133cfc0fc59fc316eab5d21a8e
tutok <walas.marcin@gmail.com>
2019-11-18 10:52:53 +0100

    feat(checkout-providers): added usePlaceOrder hook and coresponding PlaceOrder component

---
+++ packages/falcon-front-kit/src/Checkout/PlaceOrder.tsx
@@ -0,0 +1,44 @@
+import React, { useCallback } from 'react';
+import { usePlaceOrderMutation, PlaceOrderResponse } from '@deity/falcon-shop-data';
+import { addressToCheckoutAddressInput } from './CheckoutAddress';
+import { OrderData } from './CheckoutContext';
+import { useCheckout } from './CheckoutConsumer';
+import { CheckoutOperation, CheckoutOperationHook } from './CheckoutOperation';
+
+export const usePlaceOrder: CheckoutOperationHook<PlaceOrderResponse, OrderData | undefined> = () => {
+  const { setLoading, placeOrder, setResult } = useCheckout();
+  const [mutation, mutationResult] = usePlaceOrderMutation({
+    onCompleted: data => {
+      setResult(data.placeOrder);
+      setLoading(false);
+    }
+  });
+
+  return [
+    useCallback(async (input: OrderData, options) => {
+      setLoading(true);
+      placeOrder(input);
+
+      return mutation({
+        ...(options || {}),
+        variables: {
+          input: {
+            email: input.email,
+            billingAddress: addressToCheckoutAddressInput(input.billingAddress),
+            shippingAddress: addressToCheckoutAddressInput(input.shippingAddress),
+            shippingMethod: input.shippingMethod,
+            paymentMethod: input.paymentMethod
+          }
+        }
+      });
+    }, []),
+    mutationResult
+  ];
+};
+
+export type PlaceOrderProps = {
+  children: CheckoutOperation<PlaceOrderResponse, OrderData>;
+};
+export const PlaceOrder: React.FC<PlaceOrderProps> = ({ children }) => {
+  return children(...usePlaceOrder());
+};
