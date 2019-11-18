d09057d37d4dba4930a1c75426a5d3f4ba1b0e3d
tutok <walas.marcin@gmail.com>
2019-11-18 10:56:16 +0100

    feat(checkout-providers): simplified checkout context

--- packages/falcon-front-kit/src/Checkout/CheckoutContext.tsx
+++ packages/falcon-front-kit/src/Checkout/CheckoutContext.tsx
@@ -1,41 +1,30 @@
 import React from 'react';
-import { PlaceOrderResult, CheckoutDetailsInput, ShippingMethod, PaymentMethod } from '@deity/falcon-shop-extension';
+import { CheckoutDetailsInput, PlaceOrderResult } from '@deity/falcon-shop-extension';
 import { CheckoutAddress } from './CheckoutAddress';
 import { CheckoutState } from './CheckoutState';
 
-type CheckoutContextError = {
-  message: string;
-};
-
-export type CheckoutContextData = {
-  errors: CheckoutContextErrors;
-  result?: PlaceOrderResult;
-  availableShippingMethods: ShippingMethod[];
-  availablePaymentMethods: PaymentMethod[];
-};
-
-type CheckoutContextErrors = {
-  email?: CheckoutContextError[];
-  shippingAddress?: CheckoutContextError[];
-  billingSameAsShipping?: CheckoutContextError[];
-  billingAddress?: CheckoutContextError[];
-  shippingMethod?: CheckoutContextError[];
-  paymentMethod?: CheckoutContextError[];
-  order?: CheckoutContextError[];
+export type OrderData = {
+  email?: string;
+  billingAddress: CheckoutAddress;
+  shippingAddress: CheckoutAddress;
+  paymentMethod: CheckoutDetailsInput; // TODO: use `PaymentMethod` type?
+  shippingMethod: CheckoutDetailsInput; // TODO: use `ShippingMethod` type?
 };
 
 export type CheckoutProviderRenderProps = {
-  values: CheckoutState;
   isLoading: boolean;
   setLoading(isLoading: boolean);
+  values: CheckoutState;
   setEmail(email: string): void;
   setShippingAddress(address: CheckoutAddress): void;
   setBillingSameAsShipping(same: boolean): void;
   setBillingAddress(address: CheckoutAddress): void;
   setShippingMethod(shipping: CheckoutDetailsInput): void;
   setPaymentMethod(payment: CheckoutDetailsInput): void;
-  placeOrder(): void;
-} & CheckoutContextData;
+  placeOrder(result: OrderData): void;
+  result?: PlaceOrderResult;
+  setResult(result: PlaceOrderResult): void;
+};
 
 export type CheckoutContextType = {};
 
--- packages/falcon-front-kit/src/Checkout/CheckoutProvider.tsx
+++ packages/falcon-front-kit/src/Checkout/CheckoutProvider.tsx
@@ -1,25 +1,8 @@
 import React, { useState } from 'react';
 import isEqual from 'lodash.isequal';
-import { CheckoutDetailsInput } from '@deity/falcon-shop-extension';
-import {
-  // Step 1
-  useSetShippingAddressMutation,
-  // Step 2
-  useSetBillingAddressMutation,
-  // Step 3
-  useShippingMethodListLazyQuery,
-  // Step 4
-  useSetShippingMethodMutation,
-  // Step 5
-  usePaymentMethodListLazyQuery,
-  // Step 6
-  useSetPaymentMethodMutation,
-  // Step 7
-  usePlaceOrderMutation
-} from '@deity/falcon-shop-data';
-import { CheckoutContext, CheckoutProviderRenderProps, CheckoutContextData } from './CheckoutContext';
+import { PlaceOrderResult } from '@deity/falcon-shop-extension';
 import { CheckoutState } from './CheckoutState';
-import { CheckoutAddress, addressToCheckoutAddressInput } from './CheckoutAddress';
+import { CheckoutContext, CheckoutProviderRenderProps } from './CheckoutContext';
 
 export type CheckoutProviderProps = {
   initialValues?: CheckoutState;
@@ -34,160 +17,10 @@ export const CheckoutProvider: React.SFC<CheckoutProviderProps> = props => {
     billingSameAsShipping: false,
     ...initialValues
   });
+  const [result, setResult] = useState<PlaceOrderResult>();
 
-  const [contextData, setContextData] = useState<CheckoutContextData>({
-    errors: {},
-    availablePaymentMethods: [],
-    availableShippingMethods: []
-  });
-  const [setShippingAddressMutation, { loading: setShippingAddressLoading }] = useSetShippingAddressMutation({
-    onError: error => {
-      setPartialState({
-        errors: { shippingAddress: [error] },
-        availableShippingMethods: []
-      });
-      setState({
-        ...state,
-        ...{
-          shippingAddress: undefined,
-          ...(state.billingSameAsShipping && {
-            billingAddress: undefined
-          })
-        }
-      });
-    },
-    onCompleted: data => {
-      if (data.setShippingAddress) {
-        setPartialState({ errors: {} });
-      }
-    }
-  });
-  const [setBillingAddressMutation, { loading: setBillingAddressLoading }] = useSetBillingAddressMutation({
-    onError: error => {
-      setPartialState({
-        errors: { billingAddress: [error] },
-        availableShippingMethods: []
-      });
-      setState({
-        ...state,
-        billingAddress: undefined
-      });
-    },
-    onCompleted: data => {
-      if (data.setBillingAddress) {
-        setPartialState({ errors: {} });
-        if (shippingMethodListProps.refetch) {
-          shippingMethodListProps.refetch();
-        } else {
-          loadShippingMethodList();
-        }
-      }
-    }
-  });
-  const [loadShippingMethodList, shippingMethodListProps] = useShippingMethodListLazyQuery({
-    onError: error => {
-      setPartialState({
-        errors: { shippingMethod: [error] },
-        availableShippingMethods: []
-      });
-    },
-    onCompleted: data => {
-      const values: Partial<CheckoutState> = {};
-      // if shipping methods has changed then remove already selected shipping method
-      if (!isEqual(data.shippingMethodList, contextData.availableShippingMethods)) {
-        values.shippingMethod = undefined;
-      }
-
-      setPartialState({
-        errors: {},
-        availableShippingMethods: data.shippingMethodList
-      });
-      setState({
-        ...state,
-        ...values
-      });
-    }
-  });
-  const [setShippingMethodMutation, { loading: setShippingMethodLoading }] = useSetShippingMethodMutation({
-    onError: error => {
-      setPartialState({
-        errors: { shippingMethod: [error] },
-        availablePaymentMethods: null
-      });
-      setState({
-        ...state,
-        shippingAddress: undefined
-      });
-    },
-    onCompleted: data => {
-      if (data.setShippingMethod) {
-        setPartialState({ errors: {} });
-        if (paymentMethodListProps.refetch) {
-          paymentMethodListProps.refetch();
-        } else {
-          loadPaymentMethodList();
-        }
-      }
-    }
-  });
-  const [loadPaymentMethodList, paymentMethodListProps] = usePaymentMethodListLazyQuery({
-    onError: error => {
-      setPartialState({
-        errors: { paymentMethod: [error] },
-        availablePaymentMethods: []
-      });
-    },
-    onCompleted: data => {
-      const values: Partial<CheckoutState> = {};
-      if (!isEqual(data.paymentMethodList, contextData.availablePaymentMethods)) {
-        values.paymentMethod = undefined;
-      }
-
-      setPartialState({
-        errors: {},
-        availablePaymentMethods: data.paymentMethodList
-      });
-      setState({
-        ...state,
-        ...values
-      });
-    }
-  });
-  const [setPaymentMethodMutation, { loading: setPaymentMethodLoading }] = useSetPaymentMethodMutation({
-    onError: error => {
-      setPartialState({
-        errors: { paymentMethod: [error] }
-      });
-      setState({
-        ...state,
-        paymentMethod: undefined
-      });
-    },
-    onCompleted: () => {
-      setPartialState({ errors: {} });
-    }
-  });
-  const [placeOrderMutation, { loading: placeOrderLoading }] = usePlaceOrderMutation({
-    onError: error => {
-      setPartialState({ errors: { order: [error] } });
-    },
-    onCompleted: data => {
-      setPartialState({
-        errors: {},
-        result: data.placeOrder
-      });
-    }
-  });
-
-  const setPartialState = (partial: Partial<CheckoutContextData>) => {
-    setContextData({
-      // "deep replace" - replace old values with new, don't merge these
-      ...contextData,
-      ...partial
-    });
-  };
-
-  const setEmail = email => setState(x => (x.email === email ? x : { ...x, email }));
+  const setEmail: CheckoutProviderRenderProps['setEmail'] = email =>
+    setState(x => (x.email === email ? x : { ...x, email }));
 
   /** @param {boolean} same Whether the billing address should be the same as shipping address */
   const setBillingSameAsShipping = (same: boolean) => {
@@ -199,7 +32,7 @@ export const CheckoutProvider: React.SFC<CheckoutProviderProps> = props => {
     // setBillingAddress(state.shippingAddress);
   };
 
-  const setShippingAddress = shippingAddress =>
+  const setShippingAddress: CheckoutProviderRenderProps['setShippingAddress'] = shippingAddress =>
     setState(x =>
       isEqual(x.shippingAddress, shippingAddress)
         ? x
@@ -212,7 +45,7 @@ export const CheckoutProvider: React.SFC<CheckoutProviderProps> = props => {
           }
     );
 
-  const setBillingAddress = billingAddress =>
+  const setBillingAddress: CheckoutProviderRenderProps['setBillingAddress'] = billingAddress =>
     setState(x =>
       isEqual(x.billingAddress, billingAddress)
         ? x
@@ -223,7 +56,7 @@ export const CheckoutProvider: React.SFC<CheckoutProviderProps> = props => {
           }
     );
 
-  const setShippingMethod = shippingMethod =>
+  const setShippingMethod: CheckoutProviderRenderProps['setShippingMethod'] = shippingMethod =>
     setState(x =>
       isEqual(x.shippingMethod, shippingMethod)
         ? x
@@ -234,21 +67,17 @@ export const CheckoutProvider: React.SFC<CheckoutProviderProps> = props => {
           }
     );
 
-  const setPaymentMethod = paymentMethod =>
+  const setPaymentMethod: CheckoutProviderRenderProps['setPaymentMethod'] = paymentMethod =>
     setState(x => (isEqual(x.paymentMethod, paymentMethod) ? x : { ...x, paymentMethod }));
 
-  const placeOrder = (/** TODO: I would like to pass all necessary date here to do 1 click checkout */) => {
-    placeOrderMutation({
-      variables: {
-        input: {
-          email: state.email,
-          billingAddress: addressToCheckoutAddressInput(state.billingAddress),
-          shippingAddress: addressToCheckoutAddressInput(state.shippingAddress),
-          shippingMethod: state.shippingMethod,
-          paymentMethod: state.paymentMethod
-        }
-      }
-    });
+  const placeOrder: CheckoutProviderRenderProps['placeOrder'] = order => {
+    if (order) {
+      setEmail(order.email);
+      setBillingAddress(order.billingAddress);
+      setShippingAddress(order.shippingAddress);
+      setShippingMethod(order.shippingMethod);
+      setPaymentMethod(order.paymentMethod);
+    }
   };
 
   const context: CheckoutProviderRenderProps = {
@@ -261,7 +90,9 @@ export const CheckoutProvider: React.SFC<CheckoutProviderProps> = props => {
     setBillingAddress,
     setShippingMethod,
     setPaymentMethod,
-    placeOrder
+    placeOrder,
+    result,
+    setResult
   };
 
   return <CheckoutContext.Provider value={context}>{children}</CheckoutContext.Provider>;
