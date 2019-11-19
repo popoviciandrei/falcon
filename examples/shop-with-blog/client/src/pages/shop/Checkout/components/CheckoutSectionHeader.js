92030dad73d183648943e0df86177c9fb40abe2e
tutok <walas.marcin@gmail.com>
2019-11-19 17:12:59 +0100

    feat(checkout-providers): cleanup

--- examples/shop-with-blog/client/src/pages/shop/Checkout/components/CheckoutSectionHeader.js
+++ examples/shop-with-blog/client/src/pages/shop/Checkout/components/CheckoutSectionHeader.js
@@ -3,15 +3,10 @@ import PropTypes from 'prop-types';
 import { Box, H2, Button, Summary, Icon } from '@deity/falcon-ui';
 import { CheckoutSectionHeaderLayout, CheckoutSectionHeaderLayoutArea } from './CheckoutSectionHeaderLayout';
 
-export const CheckoutSectionHeader = ({ complete, open, title, summary, editLabel, onActionClick }) => (
+export const CheckoutSectionHeader = ({ title, complete, open, summary, editLabel, onActionClick }) => (
   <Summary onClick={e => e.preventDefault()} defaultTheme={CheckoutSectionHeaderLayout}>
     {(complete || open) && (
-      <Icon
-        size="lg"
-        gridArea={CheckoutSectionHeaderLayoutArea.icon}
-        src={complete ? 'check' : 'arrowRight'}
-        css={{ cursor: 'default' }}
-      />
+      <Icon size="lg" gridArea={CheckoutSectionHeaderLayoutArea.icon} src={complete ? 'check' : 'arrowRight'} />
     )}
     <H2 fontSize="lg" gridArea={CheckoutSectionHeaderLayoutArea.title}>
       {title}
@@ -35,9 +30,9 @@ export const CheckoutSectionHeader = ({ complete, open, title, summary, editLabe
   </Summary>
 );
 CheckoutSectionHeader.propTypes = {
+  title: PropTypes.string.isRequired,
   complete: PropTypes.bool,
   open: PropTypes.bool,
-  title: PropTypes.string,
   summary: PropTypes.shape({}),
   editLabel: PropTypes.string,
   onActionClick: PropTypes.func
