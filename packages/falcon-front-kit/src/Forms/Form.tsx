b89f8856cfb1a66a8938399382d87c7516689cfa
tutok <walas.marcin@gmail.com>
2019-11-20 15:25:04 +0100

    feat(checkout-providers): added autosubmit to Form component

--- packages/falcon-front-kit/src/Forms/Form.tsx
+++ packages/falcon-front-kit/src/Forms/Form.tsx
@@ -1,15 +1,39 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import { Form as FormikForm, FormikFormProps } from 'formik';
+import { Form as FormikForm, FormikFormProps, useFormikContext } from 'formik';
+import { Loader } from '@deity/falcon-data';
 import { FormContext, FormContextValue } from './FormContext';
 
-export type FormProps = FormContextValue & FormikFormProps;
+export type FormProps = FormContextValue &
+  FormikFormProps & {
+    autoSubmit: boolean;
+  };
 
-export const Form: React.SFC<FormProps> = ({ i18nId, ...restProps }) => (
-  <FormContext.Provider value={{ id: restProps.id, name: restProps.name, i18nId }}>
-    <FormikForm {...restProps} />
-  </FormContext.Provider>
-);
+export const Form: React.SFC<FormProps> = ({ i18nId, autoSubmit, ...restProps }) => {
+  const { submitCount, isValid, submitForm, isSubmitting } = useFormikContext();
+
+  if (autoSubmit) {
+    if (!isSubmitting && submitCount === 0 && isValid) {
+      submitForm();
+      return null;
+    }
+    if (isSubmitting && submitCount <= 1) {
+      return <Loader />;
+    }
+  }
+
+  return (
+    <FormContext.Provider
+      value={{
+        id: restProps.id,
+        name: restProps.name,
+        i18nId
+      }}
+    >
+      <FormikForm {...restProps} />
+    </FormContext.Provider>
+  );
+};
 Form.propTypes = {
   id: PropTypes.string.isRequired
 };
