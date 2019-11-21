23a2a564bcdaa18ffc21b1ba379b07e30bb8e74b
tutok <walas.marcin@gmail.com>
2019-11-21 11:44:11 +0100

    feat(checkout-providers): simplified FormSubmit

--- packages/falcon-ui-kit/src/Forms/FormSubmit.tsx
+++ packages/falcon-ui-kit/src/Forms/FormSubmit.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
-import { FormikProps, useFormik, useFormikContext } from 'formik';
-import { Button, BoxProps, extractThemableProps, Box } from '@deity/falcon-ui';
+import { FormikProps } from 'formik';
+import { Button, BoxProps, extractThemableProps } from '@deity/falcon-ui';
 import { Submit } from '@deity/falcon-front-kit';
 
 export type FormSubmitRenderProps<TValue = any> = {
@@ -12,33 +12,24 @@ export type FormSubmitRenderProps<TValue = any> = {
   };
 };
 
-export type FormSubmitProps<TValue = any> = {
-  value?: string;
-  children?: (props: FormSubmitRenderProps<TValue>) => React.ReactNode;
-} & React.ButtonHTMLAttributes<HTMLButtonElement> &
-  BoxProps;
+export type FormSubmitProps = {} & React.ButtonHTMLAttributes<HTMLButtonElement> & BoxProps;
 
 export const FormSubmit: React.SFC<FormSubmitProps> = ({ value, children, ...restProps }) => {
   const { themableProps, rest } = extractThemableProps(restProps);
 
   return (
     <Submit {...rest}>
-      {({ form, submit }) =>
-        children ? (
-          children({ form, submit })
-        ) : (
-          <Box {...themableProps}>
-            <Button
-              type="submit"
-              disabled={form.isSubmitting}
-              variant={form.isSubmitting ? 'loader' : undefined}
-              {...submit}
-            >
-              {value || submit.value}
-            </Button>
-          </Box>
-        )
-      }
+      {({ form, submit: { value: submitValue, ...submitRest } }) => (
+        <Button
+          type="submit"
+          disabled={form.isSubmitting}
+          variant={form.isSubmitting ? 'loader' : undefined}
+          {...submitRest}
+          {...themableProps}
+        >
+          {children || value || submitValue}
+        </Button>
+      )}
     </Submit>
   );
 };
