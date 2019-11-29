a45da508fff09d04013b1542c1ab90cfef370b04
tutok <walas.marcin@gmail.com>
2019-11-29 15:12:32 +0100

    feat(dynamic-route-transitions): added SwitchDynamicURL component

--- packages/falcon-front-kit/src/DynamicRoute/DynamicRoute.tsx
+++ packages/falcon-front-kit/src/DynamicRoute/DynamicRoute.tsx
@@ -1,22 +1,20 @@
-import React from 'react';
-import { Redirect } from 'react-router-dom';
+import React, { useState } from 'react';
 import PropTypes from 'prop-types';
+import { Redirect, matchPath, match as Match, SwitchProps } from 'react-router-dom';
 import { Location } from 'history';
-import { UrlQuery, ResourceMeta } from '@deity/falcon-data';
+import isEqual from 'lodash.isequal';
+import { UrlQuery, ResourceMeta, useUrlLazyQuery, Loader, OperationError } from '@deity/falcon-data';
 import { Router } from '../Router';
 
 export type DynamicRouteComponentProps = Pick<ResourceMeta, 'id' | 'path'>;
-
 export type ComponentsMap = Record<string, React.ComponentType<DynamicRouteComponentProps> | object>;
-
 export type DynamicRouteProps = {
   location?: Location;
   components: ComponentsMap;
   notFound: React.ComponentType<{ location?: any }>;
 };
-
 export const DynamicRoute: React.SFC<DynamicRouteProps> = props => {
-  const { components, notFound } = props;
+  const { components, notFound: NotFound } = props;
 
   return (
     <Router>
@@ -28,8 +26,6 @@ export const DynamicRoute: React.SFC<DynamicRouteProps> = props => {
           <UrlQuery variables={{ path }}>
             {({ data: { url } }) => {
               if (!url) {
-                const NotFound = notFound;
-
                 return <NotFound location={location} />;
               }
 
@@ -54,4 +50,141 @@ DynamicRoute.propTypes = {
   location: PropTypes.any,
   components: PropTypes.objectOf(PropTypes.oneOfType([PropTypes.object, PropTypes.func]).isRequired).isRequired,
   notFound: PropTypes.func.isRequired
+};
+
+export const SwitchDynamicURL: React.FC<SwitchProps> = props => {
+  const [processing, setProcessing] = useState<boolean>(false);
+  const [previousLocation, setPreviousLocation] = useState<Location<any>>(undefined);
+  const [previousResourceMeta, setPreviousResourceMeta] = useState<ResourceMeta>(undefined);
+  const [urlQuery, { loading, error, data }] = useUrlLazyQuery({
+    onCompleted: result => {
+      if (result) {
+        setPreviousResourceMeta(result.url);
+      }
+    }
+  });
+
+  const isResourceMetaRequired = (children: React.ReactNode, location: Location): boolean => {
+    let result = false;
+    let match: Match<any> | null;
+
+    // We use React.Children.forEach instead of React.Children.toArray().find()
+    // here because toArray adds keys to all child elements and we do not want
+    // to trigger an unmount/remount for two <Route>s that render the same
+    // component at different URLs.
+    React.Children.forEach(children, child => {
+      if (!result && match == null && React.isValidElement(child)) {
+        const path = child.props.path || child.props.from;
+        if (path) {
+          match = matchPath(location.pathname, { ...child.props, path });
+        } else {
+          result = !!child.props.type;
+        }
+      }
+    });
+
+    return result;
+  };
+
+  const findRouteElement = (
+    children: React.ReactNode,
+    location: Location,
+    resourceMeta?: ResourceMeta
+  ): [Match | null, React.ReactNode] => {
+    let element: React.ReactNode;
+    let match: Match<any> | null;
+
+    // We use React.Children.forEach instead of React.Children.toArray().find()
+    // here because toArray adds keys to all child elements and we do not want
+    // to trigger an unmount/remount for two <Route>s that render the same
+    // component at different URLs.
+    React.Children.forEach(children, child => {
+      if (match == null && React.isValidElement(child)) {
+        const path = child.props.path || child.props.from;
+        if (path) {
+          match = matchPath(location.pathname, { ...child.props, path });
+        } else {
+          // eslint-disable-next-line
+          if (child.props.type && resourceMeta) {
+            match = matchPath(resourceMeta.type, { ...child.props, path: child.props.type });
+          }
+        }
+
+        // eslint-disable-next-line
+        element = child;
+      }
+    });
+
+    return [match, element];
+  };
+
+  return (
+    <Router>
+      {context => {
+        const location = props.location || context.location;
+
+        const shouldQueryUrl =
+          !processing &&
+          !loading &&
+          !isEqual(location, previousLocation) &&
+          isResourceMetaRequired(props.children, location);
+
+        if (shouldQueryUrl) {
+          urlQuery({ variables: { path: location.pathname } });
+          setProcessing(true);
+        }
+
+        if (loading || shouldQueryUrl) {
+          const [previousMatch, previousElement] = previousLocation
+            ? findRouteElement(props.children, previousLocation, previousResourceMeta)
+            : [undefined, undefined];
+
+          if (previousMatch) {
+            if (previousResourceMeta) {
+              previousMatch.params = {
+                ...previousMatch.params,
+                id: previousResourceMeta.id,
+                path: previousResourceMeta.path
+              };
+            }
+
+            return (
+              <React.Fragment>
+                <Loader variant="overlay" />
+                {React.cloneElement(previousElement as any, {
+                  location: previousLocation,
+                  computedMatch: previousMatch
+                })}
+              </React.Fragment>
+            );
+          }
+
+          return <Loader variant="overlay" />;
+        }
+
+        if (error) {
+          return <OperationError {...error} />;
+        }
+
+        setPreviousLocation(location);
+        setProcessing(false);
+        const [match, element] = findRouteElement(props.children, location, data && data.url);
+
+        const computedMatch = match || context.match;
+        if (computedMatch) {
+          if (data && data.url) {
+            computedMatch.params = {
+              ...computedMatch.params,
+              id: data.url.id,
+              path: data.url.path
+            };
+          }
+
+          return React.cloneElement(element as any, { location, computedMatch });
+        }
+
+        return null;
+      }}
+    </Router>
+  );
 };
