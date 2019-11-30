4739dd6ef343b2c6fd3b78050f038bc0ee1d3532
Nimo Beeren <nimo@deity.io>
2019-11-30 16:16:36 +0100

    improvement: remove unnecessary prettier override

--- prettier.config.js
+++ prettier.config.js
@@ -1,11 +1,3 @@
-/* eslint-disable prefer-object-spread */
 const esLint = require('@deity/eslint-config-falcon');
 
-module.exports = Object.assign({}, esLint.rules['prettier/prettier'][1], {
-  overrides: [
-    {
-      files: ['.eslintrc'],
-      options: { parser: 'json' }
-    }
-  ]
-});
+module.exports = esLint.rules['prettier/prettier'][1];
