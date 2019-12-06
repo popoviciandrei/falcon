b681e76cca0524d46e033e23ed55871a6d8bb183
tutok <walas.marcin@gmail.com>
2019-12-06 16:04:16 +0100

    feat(checkout-providers): make sure that payment method chunk will be donloaded only one, so we need to render "lodable" component only once

--- examples/shop-with-blog/client/src/pages/shop/Checkout/components/PaymentMethodItem.js
+++ examples/shop-with-blog/client/src/pages/shop/Checkout/components/PaymentMethodItem.js
@@ -1,78 +1,68 @@
 import React from 'react';
-import PropTypes from 'prop-types';
-import { FlexLayout, Label, Image, Radio } from '@deity/falcon-ui';
+import { TwoStepWizard } from '@deity/falcon-front-kit';
+import { Box, Text, FlexLayout, Label, Image, Radio } from '@deity/falcon-ui';
 import { SimplePayment } from '@deity/falcon-payment-plugin';
 import PayPal from '@deity/falcon-paypal-plugin';
 import Adyen from './payments/Adyen';
 
-const paymentPlugins = {
+const paymentCodeToPluginMap = {
   adyen_cc: Adyen,
-  paypal_express: PayPal
+  paypal_express: PayPal,
+  checkmo: SimplePayment
 };
 
-class PaymentMethodItem extends React.Component {
-  renderIcon(code) {
-    const icon = code in paymentPlugins && paymentPlugins[code].icon ? paymentPlugins[code].icon : null;
+const PaymentMethodPicker = ({ options, selected, onChange }) => {
+  const isPaymentCodeMapDefined = code => code in paymentCodeToPluginMap;
+  const getPaymentPluginFor = code => (isPaymentCodeMapDefined(code) ? paymentCodeToPluginMap[code] : undefined);
 
-    if (!icon) {
-      return null;
-    }
+  return (
+    <TwoStepWizard initialState={selected || {}}>
+      {({ selectedOption, selectOption }) => {
+        const SelectedPaymentPlugin = (selectedOption && getPaymentPluginFor(selectedOption.code)) || undefined;
 
-    return <Image src={icon} mr="xs" mb="xs" css={{ verticalAlign: 'middle' }} />;
-  }
+        return (
+          <React.Fragment>
+            <Box>
+              {options.map(method => {
+                const paymentPlugin = getPaymentPluginFor(method.code);
+                if (!paymentPlugin) {
+                  console.error(`No Payment Method Plugin found for ${method.code}`);
+                  return;
+                }
 
-  renderPlugin() {
-    const { selectedOption, config, code, onPaymentDetailsReady } = this.props;
-
-    if (!selectedOption || selectedOption !== code) {
-      return null;
-    }
-
-    const paymentProps = { config, onPaymentDetailsReady };
-    switch (selectedOption) {
-      case 'adyen_cc':
-        return <Adyen {...paymentProps} />;
-      case 'paypal_express':
-        return <PayPal {...paymentProps} />;
-      default:
-        // For the rest unmapped payments - return "SimplePayment" plugin
-        return <SimplePayment {...paymentProps} />;
-    }
-  }
-
-  render() {
-    const { code, title, selectOption } = this.props;
-
-    return (
-      <FlexLayout my="xs" css={{ alignItems: 'center' }}>
-        <Radio size="sm" name="payment" id={`opt-${code}`} value={code} onChange={() => selectOption(code)} />
-        <Label mx="sm" flex="1" htmlFor={`opt-${code}`}>
-          {this.renderIcon(code)}
-          {title}
-        </Label>
-        {this.renderPlugin()}
-      </FlexLayout>
-    );
-  }
-}
-
-PaymentMethodItem.propTypes = {
-  // Payment code
-  code: PropTypes.string.isRequired,
-  // Payment title
-  title: PropTypes.string.isRequired,
-  // Payment config (for plugin)
-  config: PropTypes.shape({}),
-  // Currently selected payment method
-  selectedOption: PropTypes.string,
-  // Payment method selector callback
-  selectOption: PropTypes.func.isRequired,
-  // Payment details ready callback
-  onPaymentDetailsReady: PropTypes.func.isRequired
+                return (
+                  <FlexLayout key={method.code} my="sm" alignItems="center">
+                    <Radio
+                      id={`opt-${method.code}`}
+                      name="payment"
+                      value={method.code}
+                      checked={method.code === selectedOption.code}
+                      onChange={() => selectOption(method)}
+                      size="sm"
+                    />
+                    <Label flex="1" display="flex" alignItems="center" ml="sm" htmlFor={`opt-${method.code}`}>
+                      {paymentPlugin.icon && <Image src={paymentPlugin.icon} css={{ verticalAlign: 'middle' }} />}
+                      <Text as="span" p="xs">
+                        {method.title}
+                      </Text>
+                    </Label>
+                  </FlexLayout>
+                );
+              })}
+            </Box>
+            {SelectedPaymentPlugin && (
+              <Box>
+                <SelectedPaymentPlugin
+                  config={selectedOption.config}
+                  onPaymentDetailsReady={details => onChange({ ...selectedOption, data: details })}
+                />
+              </Box>
+            )}
+          </React.Fragment>
+        );
+      }}
+    </TwoStepWizard>
+  );
 };
 
-PaymentMethodItem.defaultProps = {
-  config: {}
-};
-
-export default PaymentMethodItem;
+export default PaymentMethodPicker;
