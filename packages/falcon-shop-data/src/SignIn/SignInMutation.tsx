139e924b1df72196b9ea5d8fbf47a24caab5e91c
tutok <walas.marcin@gmail.com>
2019-12-13 12:56:03 +0100

    feat(refetch-queries-fix): update customer inside apollo store when signin/signout

--- packages/falcon-shop-data/src/SignIn/SignInMutation.tsx
+++ packages/falcon-shop-data/src/SignIn/SignInMutation.tsx
@@ -1,27 +1,38 @@
 import gql from 'graphql-tag';
 import { useMutation, MutationHookOptions } from '@apollo/react-hooks';
 import { Mutation, OperationInput } from '@deity/falcon-data';
-import { SignInInput } from '@deity/falcon-shop-extension';
+import { SignInInput, Customer } from '@deity/falcon-shop-extension';
+import { GET_IS_AUTHENTICATED } from '../Customer';
 
 export const SIGN_IN = gql`
   mutation SignIn($input: SignInInput!) {
-    signIn(input: $input)
+    signIn(input: $input) {
+      id
+    }
   }
 `;
 
-export type SignInResponse = { signIn: boolean };
+export type SignInResponse = { signIn: Pick<Customer, 'id'> };
+
+const defaultOptions = {
+  refetchQueries: ['Cart', 'Customer'],
+  awaitRefetchQueries: false,
+  update: (store, { data: { signIn: customer } }) => {
+    const data = store.readQuery({ query: GET_IS_AUTHENTICATED });
+    data.customer = customer;
+    store.writeQuery({ query: GET_IS_AUTHENTICATED, data: { ...data } });
+  }
+};
 
 export class SignInMutation extends Mutation<SignInResponse, OperationInput<SignInInput>> {
   static defaultProps = {
     mutation: SIGN_IN,
-    refetchQueries: ['Cart', 'CustomerWithAddresses', 'Customer'],
-    awaitRefetchQueries: false
+    ...defaultOptions
   };
 }
 
 export const useSignInMutation = (options: MutationHookOptions<SignInResponse, OperationInput<SignInInput>> = {}) =>
   useMutation(SIGN_IN, {
-    refetchQueries: ['Cart', 'CustomerWithAddresses', 'Customer'],
-    awaitRefetchQueries: false,
+    ...defaultOptions,
     ...options
   });
--- packages/falcon-shop-data/src/SignIn/SignOutMutation.tsx
+++ packages/falcon-shop-data/src/SignIn/SignOutMutation.tsx
@@ -1,6 +1,7 @@
 import gql from 'graphql-tag';
 import { useMutation, MutationHookOptions } from '@apollo/react-hooks';
 import { Mutation } from '@deity/falcon-data';
+import { GET_IS_AUTHENTICATED } from '../Customer';
 
 export const SIGN_OUT = gql`
   mutation SignOut {
@@ -10,17 +11,25 @@ export const SIGN_OUT = gql`
 
 export type SignOutResponse = { signOut: boolean };
 
+const defaultOptions = {
+  refetchQueries: ['Customer', 'Cart'],
+  awaitRefetchQueries: false,
+  update: store => {
+    const data = store.readQuery({ query: GET_IS_AUTHENTICATED });
+    data.customer = undefined;
+    store.writeQuery({ query: GET_IS_AUTHENTICATED, data });
+  }
+};
+
 export class SignOutMutation extends Mutation<SignOutResponse> {
   static defaultProps = {
     mutation: SIGN_OUT,
-    refetchQueries: ['Customer', 'Cart'],
-    awaitRefetchQueries: false
+    ...defaultOptions
   };
 }
 
 export const useSignOutMutation = (options: MutationHookOptions<SignOutResponse> = {}) =>
   useMutation(SIGN_OUT, {
-    refetchQueries: ['Customer', 'Cart'],
-    awaitRefetchQueries: false,
+    ...defaultOptions,
     ...options
   });
